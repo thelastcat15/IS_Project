ec0829c8ce9375142e9ec439ece251eacb4fb212
thelastcat15 <smart000firstfenix@gmail.com>
2025-03-17 00:03:00 +0700

    setup

--- train_nn.py
+++ train_nn.py
@@ -1,4 +1,3 @@
-import os
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
